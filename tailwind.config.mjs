ba3ca6714640cf55a8a18be0c44af37ae6617501
ale <alejandro79861@gmail.com>
2024-02-15 20:57:38 -0500

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			'blue1': '#0488DF',
+			'black': '#000000',
+			'grayBg': '#F1F3F0',
+			'grayTexts': '#A1A0A8',
+			'white':'#fff'
+		  },
+		extend: {},
+	},
+	plugins: [],
+}
